5af31f05c782429b8b4975dae43f8796f0a23b1f
igor__isay <igor.isay.mailbox@gmail.com>
2022-06-15 00:20:22 +0300

    escape dollar characters fix

--- delete.sh
+++ delete.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-source ./config.sh
+sed 's/\$/\\$/g' config.sh > config.tmp.sh
+source ./config.tmp.sh
+rm config.tmp.sh
 
 WP_INSTALLER_DB_NAME=$(echo "$WP_INSTALLER_SITE_NAME" | sed -e 's/[^a-zA-Z0-9]//g')
 mysql -u $WP_INSTALLER_DB_USER -p$WP_INSTALLER_DB_PASSWORD <<QUERY
